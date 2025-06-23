9e734d60f1be8fa436b42365a5bdce404b8fda83
Maxim Nikolsky <maxim.nikolskiy@fors.ru>
2025-06-23 18:45:10 +0300

    Задание 3. Репликация

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -1,52 +1,45 @@
 #!/bin/bash
 
-docker exec -i configSrv mongosh --port 27017 <<EOF
+docker exec -i configSrv1 mongosh --port 27031 <<EOF
 rs.initiate(
   {
     _id : "config_server",
     configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27017" }
+      { _id : 0, host : "configSrv1:27031" },
+      { _id : 1, host : "configSrv2:27032" },
+      { _id : 2, host : "configSrv3:27033" },
     ]
   }
 );
 exit();
 EOF
 
-docker exec -i shard1 mongosh --port 27018 <<EOF
+docker exec -i shard1_1 mongosh --port 27011 <<EOF
 rs.initiate(
   {
     _id : "shard1",
     members: [
-      { _id : 0, host : "shard1:27018" }
+      { _id : 10, host : "shard1_1:27011" },
+      { _id : 11, host : "shard1_2:27012" },
+      { _id : 12, host : "shard1_3:27013" }
     ]
   }
 );
 exit();
 EOF
 
-docker exec -i shard2 mongosh --port 27019 <<EOF
+docker exec -i shard2_1 mongosh --port 27021 <<EOF
 rs.initiate(
   {
     _id : "shard2",
     members: [
-      { _id : 1, host : "shard2:27019" }
+      { _id : 20, host : "shard2_1:27021" },
+      { _id : 21, host : "shard2_2:27022" },
+      { _id : 22, host : "shard2_3:27023" },
     ]
   }
 );
 exit();
-EOF
-
-docker exec -i mongos_router mongosh --port 27020 <<EOF
-sh.addShard("shard1/shard1:27018");
-sh.addShard("shard2/shard2:27019");
-sh.enableSharding("somedb");
-sh.shardCollection("somedb.helloDoc", { "name" : "hashed" });
-use somedb;
-for (var i = 0; i < 1000; i++) {
-  db.helloDoc.insert({ age: i, name: "ly" + i })
-};
-db.helloDoc.countDocuments();
-exit()
 EOF
 
